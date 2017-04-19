b6d7a04755636de0a6d8874af299e3fa4fff081a
daveyx <d.rehle@gmail.com>
2017-04-19 02:42:02 +0200

    setup app

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,8 +2,9 @@
 
 import React from 'react';
 import ReactDOM from "react-dom";
+import App from './components/app';
 
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <App />,
   document.getElementById('root')
 );
